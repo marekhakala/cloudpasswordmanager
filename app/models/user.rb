febd5d3843b03e23370910cdb8babf371bb6021c
marekhakala <hakala.marek@gmail.com>
2016-12-04 16:12:32 +0100

    Added support for auth by Devise and Doorkeeper

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :fullname, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def is_current_user?
+    return current_user == self
+  end
+end
